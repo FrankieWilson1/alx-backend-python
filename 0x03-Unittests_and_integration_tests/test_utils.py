13cb0282519a4e19e0b2e256edc1c34d747db86d
frankiewilson1 <frankwilliamsugwu@gmail.com>
2025-07-21 15:22:55 -0700

    FEAT: Add TestMemoize class for testing memoization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ Unit tests for the access_nested_map function and get_json functions in utils.py
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -38,6 +38,7 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), f"'{expected_message}'")
 
+# 2. --- Mock HTTP Calls ---
 
 class TestGetJson(unittest.TestCase):
     """
@@ -60,3 +61,54 @@ class TestGetJson(unittest.TestCase):
         # Assertions
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+# 3. --- Parameterized and Patch ----
+
+class TestMemoize(unittest.TestCase):
+    """
+    Tests for the memoize decorator.
+    """
+    
+    def test_memoize(self):
+        """
+        Tests that a method decorated with @memoize is called only once
+        even if accessed multiple times.
+        """
+        class TestClass:
+            """
+            A test class to demonstrate the memoization.
+            """
+            
+            def a_method(self):
+                """
+                A simple method that returns 42.
+                This method will be mocked to track calls.
+                """
+                return 42
+    
+            @memoize
+            def a_property(self):
+                """"
+                A property that memoize the result of a_method.
+                """
+                return self.a_method()
+    
+        # Tracks if 'a_method' is called and controls it's return value.
+        with patch.object(TestClass, 'a_method') as mock_a_method:
+            mock_a_method.return_value = 42
+            
+            # An instace of TestClass
+            test_instance = TestClass()
+            
+            # Access the memoized property twice
+            reuslt1 = test_instance.a_property
+            reuslt2 = test_instance.a_property
+            
+            # Assertions:
+            # Case: memoized property returns the correct value on both accesses.
+            self.assertEqual(reuslt1, 42)
+            self.assertEqual(reuslt2, 42)
+            
+            # Original method called once.
+            mock_a_method.assert_called_once()
