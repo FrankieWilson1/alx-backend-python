54f6a0def57fc1abff6baa59f06a0e1129463fc7
frankiewilson1 <frankwilliamsugwu@gmail.com>
2025-07-27 03:03:41 -0700

    Fix: Finalize nested message routing in chats/urls.py for Task 4

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,18 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet, UserViewSet
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.registers.NestedDefault(router, r'conversation=message')
+
+
 router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-	path('', include(router.urls))
+	path('', include(router.urls)),
+ path('', include(conversations_router.urls)),
 ]
