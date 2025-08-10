97951a81428fb87170d86359293610ff7774b113
frankiewilson1 <frankwilliamsugwu@gmail.com>
2025-08-10 13:16:23 +0100

    hot fix

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
+from django.db.models import Q
+from django.contrib.auth import get_user_model
 
 from .models import Message
 
+
+@login_required
+def send_message(request: HttpRequest) -> HttpResponse:
+    """
+    A view to handle sending a new message.
+    """
+    if request.method: == 'POST':
+        receiver_id = request.POST.get('receiver')
+        content = request.POST.get('content')
+        parent_message_id = request.POST.get('parent_message')
+
+        if receiver_id and content:
+            receiver_user = get_object_or_404(
+                User, user_id=receiver_id
+            )
+            message = Message.objects.create(
+                sender=request.user,
+                receiver=receiver_user,
+                content=content,
+                parent_message_id=parent_message_id if parent_message_id else None
+            )
+    return redirect('home')
 
 @login_required
 def message_detail(request, message_id):
